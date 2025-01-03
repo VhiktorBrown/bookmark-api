78b54002c83d11456e46148830100424dffdb63b
Victor <bukkychukwujekwu@gmail.com>
2025-01-03 07:08:46 +0100

    Finished the sign up function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
 import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable({})
 export class AuthService {
@@ -11,22 +12,42 @@ export class AuthService {
         //first hash user's password.
         const hash = await argon.hash(dto.password);
 
-        //then, save user in the database.
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
+        try {
+            //then, save user in the database.
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                }
+            });
+
+            //remove the hash from the response
+            delete user.hash;
+
+            //and return the saved user as a response.
+            return user;
+        }catch(error){
+            if(error 
+                instanceof 
+                PrismaClientKnownRequestError){
+                if(error.code == 'P2002'){
+                    throw new ForbiddenException(
+                        'Account already exists');
+                }
             }
-        });
-
-        //remove the hash from the response
-        delete user.hash;
-
-        //and return the saved user as a response.
-        return user;
+            throw error;
+        }
+        
     }
 
-    signIn(){
-        return {message: 'I am signed in'};
+    async signIn(dto: AuthDto){
+        //first, find the user
+        //then, if user does not exist, throw an exception
+
+        //compare passwords
+        //if password is incorrect, throw an exception
+
+        //send back the user.
+
     }
 }
